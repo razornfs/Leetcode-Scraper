c5c46e2fa2e7a71a3f424343598cf440e2bd725b
Jakub Orzołek <kubaorz1@gmail.com>
2017-02-22 17:17:24 +0100

    removed all static methods

--- LeetcodeScraper.java
+++ LeetcodeScraper.java
@@ -5,53 +5,47 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.io.Console;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
 public class LeetcodeScraper {
 
-    private static final String loginPage = "http://leetcode.com/accounts/login/";
-    private static String username;
-    private static String password;
-    private static final int timeout = 2;
-    private static WebDriver webDriver;
+    private final String loginPage = "http://leetcode.com/accounts/login/";
+    private final int timeout = 2;
 
-    static {
+    private String username;
+    private String password;
+    private WebDriver webDriver;
+
+
+    void start() {
+        setUpWebDriver();
+        getUserDetails();
+        login();
+        selectSolvedFromDropdown();
+        selectAllRowsFromDropdown();
+        parseSolvedExercises();
+    }
+
+    private void setUpWebDriver() {
         System.setProperty("webdriver.chrome.driver",
                            "src/main/resources/drivers/chrome/chromedriver.exe");
         webDriver = new ChromeDriver();
     }
 
-    public static void main(String[] args) {
-        getUserDetails();
-        login();
-        selectSolvedFromDropdown();
-        selectAllRowsFromDropdown();
-
-        // extract links from table
-        WebElement table = webDriver.findElement(By.className("reactable-data"));
-        List<WebElement> tableRows = table.findElements(By.tagName("tr"));
-        for (WebElement tableRow : tableRows) {
-            String href = tableRow.findElement(By.tagName("a")).getAttribute("href");
-            String code = scrapeCode(href);
-        }
-    }
-
-    private static void getUserDetails() {
+    private void getUserDetails() {
         System.out.println("Username: ");
         username = new Scanner(System.in).nextLine();
         System.out.println("Password: ");
-        password = Arrays.toString(System.console().readPassword());
+        password = new Scanner(System.in).nextLine();
+        //Arrays.toString(System.console().readPassword());
     }
 
-    private static void login() {
+    private void login() {
         webDriver.get(loginPage);
         webDriver.findElement(By.id("id_login")).sendKeys(username);
         webDriver.findElement(By.id("id_password")).sendKeys(password);
@@ -59,7 +53,7 @@ public class LeetcodeScraper {
         webDriver.findElement(By.id("id_password")).submit();
     }
 
-    private static void selectSolvedFromDropdown() {
+    private void selectSolvedFromDropdown() {
         String dropdownFilterXpath =
                 "//*[@id=\"question-app\"]/div/div[2]/form/div[1]/div/select";
         webDriver.manage().timeouts().implicitlyWait(timeout, TimeUnit.SECONDS);
@@ -67,7 +61,7 @@ public class LeetcodeScraper {
         dropdownFilter.selectByValue("solved");
     }
 
-    private static void selectAllRowsFromDropdown() {
+    private void selectAllRowsFromDropdown() {
         String dropdownRowsPerPageXpath =
                 "//*[@id=\"question-app\"]/div/div[2]/div/table/tbody[2]/tr/td/span/select";
         webDriver.manage().timeouts().implicitlyWait(timeout, TimeUnit.SECONDS);
@@ -75,9 +69,38 @@ public class LeetcodeScraper {
         dropdownRowsPerPage.selectByVisibleText("all");
     }
 
-    private static String scrapeCode(String href) {
-        //TODO
-        webDriver.get(href + "?tab=Submission");
+    private void parseSolvedExercises() {
+        WebElement table = webDriver.findElement(By.className("reactable-data"));
+        List<WebElement> tableRows = table.findElements(By.tagName("tr"));
+        for (WebElement row : tableRows) {
+            String exerciseHref = row.findElement(By.tagName("a")).getAttribute("href");
+            List<Code> code = getAcceptedSolutionsFromExercise(exerciseHref);
+        }
+    }
+
+    private List<Code> getAcceptedSolutionsFromExercise(String exerciseHref) {
+        List<Code> ret = new ArrayList<>();
+
+        webDriver.get(exerciseHref + "?tab=Submission");
+        WebElement table =
+                webDriver.findElement(By.xpath("/html/body/div/div[4]/div/div/div[2]/table"));
+        List<WebElement> tableRows = table.findElements(By.tagName("tr"));
+
+        for (WebElement row : tableRows) {
+            String statusClass = row.findElement(By.tagName("a")).getAttribute("class");
+            if ("text-success status-accepted".equals(statusClass)) {
+                String submissionHref = row.findElement(By.tagName("a")).getAttribute("href");
+
+                List<WebElement> tdList = row.findElements(By.tagName("td"));
+                String language = tdList.get(tdList.size() - 1).getText();
+
+                ret.add(new Code(scrapeCode(submissionHref), language));
+            }
+        }
+        return ret;
+    }
+
+    private String scrapeCode(String submissionHref) {
         return null;
     }
 }
